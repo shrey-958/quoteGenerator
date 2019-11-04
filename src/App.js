172eb345bb6db603a491abf8acab26b64898032e
shrey-958 <shreydedhia@yahoo.in>
2019-11-04 20:39:25 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import QuoteMachine from "./QuoteMachine"
+
+function App() {
+  return (
+    <div>
+      <QuoteMachine />
+    </div>
+  );
+}
+
+export default App;
